6e829046b6c4cbc18438f6cca6f0ffb1e662c5b5
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-22 10:52:20 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 01 - PATH/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+namespace EXERCISE_01___PATH {
+    //-----> START
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            string path = @"c:\temp\myfolder\files1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " +Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("GetDirctoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: "+Path.GetFileName(path));
+            Console.WriteLine("GetExtension: "+Path.GetExtension(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetFullPAth: "+Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath"+Path.GetTempPath());
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> END
+}
